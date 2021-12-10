412f3ac05d5a1daaed2232e96b5b0bc15bb4a2f1
Young-brain <9107294252@mail.ru>
2021-12-10 20:30:06 +0300

    #2 second homework

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,4 +1,4 @@
-import cn from 'classnames'
+import cn from 'classnames';
 
 function Rating(props) {
   const {stars} = props.props;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReviewList from './components/ReviewList/ReviewList';
+import cn from 'classnames';
 import './scss/main.scss';
 
 ReactDOM.render(
